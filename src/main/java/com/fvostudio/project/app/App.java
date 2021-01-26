4636727fbe0ede8e5b3afc944a99fe35763f9f21
Esteban <59137626+EstebanBaron@users.noreply.github.com>
2021-01-26 16:31:34 +0100

    test git

--- src/main/java/com/fvostudio/project/app/App.java
+++ src/main/java/com/fvostudio/project/app/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", RUNNING on Java " + javaVersion + ".");
+        Label l = new Label("SALUT LA MIF, JavaFX " + javafxVersion + ", RUNNING on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
